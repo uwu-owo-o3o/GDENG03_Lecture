45a332150376eceb9eed8f911b69e32a437737d7
uwu-owo-o3o <lance_d_ong@dlsu.edu.ph>
2025-06-01 13:02:33 +0800

    finish rehash

--- Tutorial/Tutorial_DirectX11/RenderSystem.cpp
+++ Tutorial/Tutorial_DirectX11/RenderSystem.cpp
@@ -75,8 +75,12 @@ RenderSystem::~RenderSystem()
 
 SwapChain* RenderSystem::createSwapChain(HWND hwnd, UINT width, UINT height)
 {
-
-	return new SwapChain(hwnd, width, height, this);
+	SwapChain* sc = nullptr;
+	try {
+		sc = new SwapChain(hwnd, width, height, this);
+	}
+	catch(...) {}
+	return sc;
 }
 
 DeviceContext* RenderSystem::getImmediateDeviceContext()
@@ -86,28 +90,54 @@ DeviceContext* RenderSystem::getImmediateDeviceContext()
 
 VertexBuffer* RenderSystem::createVertexBuffer(void* list_vertices, UINT size_vertex, UINT size_list, void* shader_byte_code, UINT size_byte_shader)
 {
-	return new VertexBuffer(list_vertices, size_vertex, size_list, shader_byte_code, size_byte_shader, this);
+	VertexBuffer* vb = nullptr;
+	try {
+		vb = new VertexBuffer(list_vertices, size_vertex, size_list, shader_byte_code, size_byte_shader, this);
+	}
+	catch (...) {}
+		
+	return vb;
 }
 
 ConstantBuffer* RenderSystem::createConstantBuffer(void* buffer, UINT size_buffer)
 {
-	return new ConstantBuffer(buffer, size_buffer, this);
+	ConstantBuffer* cb = nullptr;
+	try {
+		cb = new ConstantBuffer(buffer, size_buffer, this);
+	}
+	catch (...) {}
+
+	return cb;
 }
 
 IndexBuffer* RenderSystem::createIndexBuffer(void* list_indices, UINT size_list)
 {
-	return new IndexBuffer(list_indices, size_list, this);
+	IndexBuffer* ib = nullptr;
+	try {
+		ib = new IndexBuffer(list_indices, size_list, this);
+	}
+	catch (...) {}
+
+	return ib;
 }
 
 VertexShader* RenderSystem::createVertexShader(const void* shader_byte_code, size_t byte_code_size)
 {
-	VertexShader* vs = new VertexShader(shader_byte_code, byte_code_size, this);
+	VertexShader* vs = nullptr;
+	try {
+		vs = new VertexShader(shader_byte_code, byte_code_size, this);
+	}
+	catch (...) {}
 	return vs;
 }
 
 PixelShader* RenderSystem::createPixelShader(const void* shader_byte_code, size_t byte_code_size)
 {
-	PixelShader* ps = new PixelShader(shader_byte_code, byte_code_size, this);
+	PixelShader* ps = nullptr;
+	try {
+		ps = new PixelShader(shader_byte_code, byte_code_size, this);
+	}
+	catch (...) {}
 	return ps;
 }
 
